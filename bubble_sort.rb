4f0d9d7d3a2dbdb40293b79d2698b0d1469bdec7
Mwila Kaunda <mbkcust@gmail.com>
2021-04-29 11:10:49 +0200

    Add array logic

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,1 +1,41 @@
-puts 'hello'
+def bubble_sort(arr)
+    p1 = 0
+    p2 = 1
+
+    def picker(arr, p1, p2)
+        return {
+            "v1": arr[p1],
+            "v2": arr[p2]
+        }
+    end
+
+    def swap(arr, p1, p2)
+        values = picker(arr, p1, p2)
+        # arr.delete_at(p2)
+        # arr.delete_at(p1)
+        
+
+        arr[p1] = values[:v2]
+        arr[p2] = values[:v1]
+
+        return arr
+        # isSwapped = true
+        # puts picker(arr, p1, p2)
+
+        # return values[:v1]
+    end
+
+    puts swap(arr, p1, p2)
+
+    # isSwapped = false
+    # sorted = false
+
+    
+
+    # until sorted
+    #     swap
+    #     puts arr
+    # end
+end
+
+bubble_sort([5, 4, 3, 6, 8, 7])
