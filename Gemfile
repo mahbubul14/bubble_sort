cbe9406c4131bbafa992c67fdb8f2d75136be37e
mahbubul14 <mahbub21alam2017@gmailcom>
2021-04-30 18:04:47 +0600

    Fix some rubocop error

--- Gemfile
+++ Gemfile
@@ -1,2 +1,4 @@
+# frozen_string_literal: true
+
 gem 'rubocop', '>= 1.0', '< 2.0'
 source 'https://rubygems.org'
--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,111 +1,96 @@
-def bubble_sort(arr)
-    $arr = arr
+# frozen_string_literal: true
 
+def bubble_sort(arr)
+  $arr = arr
+
+  $p1 = 0
+  $p2 = 1
+
+  $isSwapped = true
+  $sorted = false
+  $counter = 0
+
+  def picker(p1, p2)
+    {
+      "v1": $arr[p1],
+      "v2": $arr[p2]
+    }
+  end
+
+  def swap
+    values = picker($p1, $p2)
+
+    v1 = values[:v1]
+    v2 = values[:v2]
+
+    if v1 > v2
+      $arr[$p1] = v2
+      $arr[$p2] = v1
+      $isSwapped = true
+    end
+  end
+
+  def reset
     $p1 = 0
     $p2 = 1
+    $isSwapped = false
+  end
 
-    $isSwapped = true
-    $sorted = false
-    $counter = 0
+  until $sorted
+    if $p2 == $arr.length
+      if $isSwapped
+        reset
+      else
+        $sorted = true
+        return $arr
+      end
+    else
+      swap
 
-    def picker(p1, p2)
-        return {
-            "v1": $arr[p1],
-            "v2": $arr[p2]
-        }
+      $p1 += 1
+      $p2 += 1
     end
-
-    def swap()
-        values = picker($p1, $p2)  
-
-        v1 = values[:v1]
-        v2 = values[:v2]
-
-        if v1 > v2
-            $arr[$p1] = v2
-            $arr[$p2] = v1
-            $isSwapped = true
-            puts $arr
-        end
-    end
-
-    def reset
-        $p1 = 0
-        $p2 = 1
-        $isSwapped = false
-    end
-
-    until $sorted
-        if $p2 == $arr.length
-            if $isSwapped
-                reset()
-                puts $arr
-            else
-                $sorted = true
-                puts $arr
-                puts "Sorted!"
-            end
-        else
-            swap()
-
-            $p1 += 1
-            $p2 += 1
-        end
-    end
+  end
 end
 
-bubble_sort([5, 4, 3, 6,-6,3,4,5,3,3,4,-1])
+puts bubble_sort([5, 4, 3, 6, -6, 3, 4, 5, 3, 3, 4, -1])
 
 def bubble_sort_by(arr)
-    $arr = arr
+  $arr = arr
 
-    $p1 = 0
-    $p2 = 1
+  $p1 = 0
+  $p2 = 1
 
-    $isSwapped = true
-    $sorted = false
-    $counter = 0
+  $isSwapped = true
+  $sorted = false
+  $counter = 0
 
-    def picker(p1, p2)
-        return {
-            "v1": $arr[p1],
-            "v2": $arr[p2]
-        }
+  until $sorted
+    if $p2 == $arr.length
+      if $isSwapped
+        reset
+      else
+        $sorted = true
+        return arr
+      end
+    else
+      values = picker($p1, $p2)
+
+      v1 = values[:v1]
+      v2 = values[:v2]
+
+      if (yield $arr[$p1], $arr[$p2]).to_i >= 0
+        $arr[$p1] = v2
+        $arr[$p2] = v1
+        $isSwapped = true
+      end
+
+      $p1 += 1
+      $p2 += 1
     end
-
-    def reset
-        $p1 = 0
-        $p2 = 1
-        $isSwapped = false
-    end
-
-    until $sorted
-        if $p2 == $arr.length
-            if $isSwapped
-                reset()
-                puts $arr
-            else
-                $sorted = true
-                puts $arr
-                puts "Sorted!"
-            end
-        else
-            values = picker($p1, $p2)  
-
-        v1 = values[:v1]
-        v2 = values[:v2]
-
-        if (yield $arr[$p1], $arr[$p2]).to_i >= 0
-            $arr[$p1] = v2
-            $arr[$p2] = v1
-            $isSwapped = true
-            puts $arr
-        end
-
-        $p1 += 1
-        $p2 += 1
-        end
-    end
+  end
 end
 
-bubble_sort_by(["hi","hello","hey","f","dfhhkdfhk","fnhdbyejhndhgbdhg"]) { |left, right| left.length - right.length } 
+puts bubble_sort_by(%w[hi hello hey f dfhhkdfhk fnhdbyejhndhgbdhg]) { |left, right|
+  left.length - right.length
+}
